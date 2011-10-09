9b12dda2df109bfee9b5f6dfa4cf700ba33f6a31
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2011-10-09 19:22:30 +0200

    Dotfiles are now symlinked to the user home.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+function install_dotfiles() {
+    dotfiles=(bashrc gitconfig gvimrc hgrc vimrc)
+    for dotfile in $dotfiles; do
+       echo "${dotfile}"
+       ln -sf ./$dotfile ~/.$dotfile
+    done
+}
 
 # https://github.com/nestor-salceda/dotfiles/blob/master/setup
 function install_or_upgrade_vundle() {
