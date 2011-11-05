1918673aeb8c7331dd86ae98a9dec1b4a8de3d87
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2011-11-05 12:39:58 +0100

    Fixed vundle and dotfiles installation.

--- setup.sh
+++ setup.sh
@@ -2,15 +2,17 @@
 
 function install_dotfiles() {
     dotfiles=(bashrc gitconfig gvimrc hgrc vimrc)
-    for dotfile in $dotfiles; do
-       echo "${dotfile}"
-       ln -sf ./$dotfile ~/.$dotfile
+    for dotfile in ${dotfiles[@]}; do
+        source=$PWD/$dotfile
+        dest=$HOME/.$dotfile
+        echo "Installing $dotfile in $dest ..."
+        ln -sf $source $dest
     done
 }
 
 # https://github.com/nestor-salceda/dotfiles/blob/master/setup
 function install_or_upgrade_vundle() {
-    vundle_dir=./.vim/bundle/vundle
+    vundle_dir=$HOME/.vim/bundle/vundle
     if [[ ! -d $vundle_dir ]]; then
         git clone http://github.com/gmarik/vundle.git $vundle_dir
     else
