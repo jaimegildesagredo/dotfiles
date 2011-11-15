038447cbccd3b76c56985988100ed342e86802a9
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2011-11-15 20:39:22 +0100

    Extracted some installation functions in the setup script.

--- setup.sh
+++ setup.sh
@@ -2,36 +2,36 @@
 
 VIMDIR=$HOME/.vim
 
+function install_symlink() {
+    echo "Installing $1 in $2..."
+    ln -sf $1 $2
+}
+
+function install_directory() {
+    if [[ ! -d $1 ]]; then
+        echo "Installing $1..."
+        mkdir -p $1
+    fi
+}
+
 function install_dotfiles() {
     dotfiles=(bashrc gitconfig gvimrc hgrc vimrc)
     for dotfile in ${dotfiles[@]}; do
-        source=$PWD/$dotfile
-        dest=$HOME/.$dotfile
-        echo "Installing $dotfile in $dest..."
-        ln -sf $source $dest
+        install_symlink $PWD/$dotfile $HOME/.$dotfile
     done
 }
 
 function install_vimdir() {
-    if [[ ! -d $VIMDIR ]]; then
-        echo "Installing $VIMDIR..."
-        mkdir -p $VIMDIR
-    fi
+    install_directory $VIMDIR
 }
 
 function install_vim_ftplugins() {
     ftplugin_dir=$VIMDIR/ftplugin
-    if [[ ! -d $ftplugin_dir ]]; then
-        echo "Installing $ftplugin_dir..."
-        mkdir -p $ftplugin_dir
-    fi
+    install_directory $ftplugin_dir
 
     ftplugins=(css.vim html.vim javascript.vim python.vim)
     for ftplugin in ${ftplugins[@]}; do
-        source=$PWD/vim/ftplugin/$ftplugin
-        dest=$ftplugin_dir/$ftplugin
-        echo "Installing $source in $dest..."
-        ln -sf $source $dest
+        install_symlink $PWD/vim/ftplugin/$ftplugin $ftplugin_dir/$ftplugin
     done
 }
 
