c7f715ac9e7fe50820e6dfb9439bc83c441d8521
Jaime Gil de Sagredo Luna <jaimegildesagredo@gmail.com>
2011-11-18 16:33:32 +0100

    Added a function to list existing virtualenvs.

--- lib/virtualenv.sh
+++ lib/virtualenv.sh
@@ -3,7 +3,6 @@
 VIRTUALENV=`which virtualenv`
 VIRTUALENVOPTS="--clear --no-site-packages"
 
-
 function workon() {
     VIRTUALENV_NAME=${1:-`basename $PWD`}
     VIRTUALENV_HOME=$WORKON_HOME/$VIRTUALENV_NAME
@@ -15,9 +14,16 @@ function workon() {
     source $VIRTUALENV_HOME/bin/activate
 }
 
-
 function mkvirtualenv() {
     VIRTUALENV_NAME=${1:-`basename $PWD`}
     $VIRTUALENV $VIRTUALENVOPTS $WORKON_HOME/$VIRTUALENV_NAME
+}
+
+function lsvirtualenvs() {
+    for virtualenv in `ls "$WORKON_HOME"`; do
+        if [[ -d "$WORKON_HOME/$virtualenv" ]]; then
+            echo $virtualenv
+        fi
+    done
 }
 
